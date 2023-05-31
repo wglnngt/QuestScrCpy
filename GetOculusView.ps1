0196fb9702c7188b97651295eadbf096fe9d2e0c
wglnngt <wglnngt.001@gmail.com>
2023-05-31 15:47:52 +0800

    [2023/05/31] 1. Upgrade scrcpy to version 2.0.

--- GetOculusView.ps1
+++ GetOculusView.ps1
@@ -84,7 +84,7 @@ Write-Host "`tAuto push file to device and run the server, so run the scrcpy cli
 
 Write-Host "`n`n"
 Write-Host "Step 7. Specify the stream device,`n`t0 for Oculus Quest2 and `n`t1 for PICO Neo X :"
-$nSType = Read-Host "Your choice for device [0:Quest|1:PICO]"
+$nSType = Read-Host "Your choice for device [0:Quest|1:PICO4|2:PICO3]"
 
 $oShell = New-Object -com WScript.Shell
 $oLink = $oShell.CreateShortcut("$env:temp\scrcpy.lnk")
@@ -96,11 +96,17 @@ if ([int]$nSType -eq 0) {
 	$oLink.WorkingDirectory = "$PWD"
 	$oLink.Arguments = "--crop 1600:900:2017:510 -n --window-title QuestViewer -s ${strConnectDevice}"
 	$oLink.Save()
-} else {
+} elseif ([int]$nSType -eq 1) {
 	# The region of pico view is : 4320x2160
 	$oLink.IconLocation = "$PWD\neo.ico"
 	$oLink.WorkingDirectory = "$PWD"
 	$oLink.Arguments = "--crop 1680:944:230:608 -n --window-title PicoViewer -s ${strConnectDevice}"
+	$oLink.Save()
+} else {
+	# The region of pico view is : 3664x1920
+	$oLink.IconLocation = "$PWD\neo.ico"
+	$oLink.WorkingDirectory = "$PWD"
+	$oLink.Arguments = "--crop 1016:1416:520:210 -n --window-title PicoViewer -s ${strConnectDevice}"
 	$oLink.Save()
 }
 Start -NoNewWindow cmd -args "/c start /b $env:temp\scrcpy.lnk"
